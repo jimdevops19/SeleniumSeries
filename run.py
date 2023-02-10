3167293adf0656f7460cb06bf26dfee311f8767e
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-02-10 07:59:11 +0100

    temp. disabled currency and inputs to debug stuff

--- run.py
+++ run.py
@@ -2,11 +2,10 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.change_currency(currency='USD')
-    bot.select_place_to_go(input("Where you want to go ?"))
-    bot.select_dates(check_in_date=input("What is the check in date ?"),
-                     check_out_date=input("What is the check out date ?"))
-    bot.select_adults(int(input("How many people ?")))
+    # bot.change_currency(currency='USD')
+    bot.select_place_to_go("warsaw")
+    bot.select_dates(check_in_date="2023-02-20", check_out_date="2023-02-23")
+    bot.select_adults(3)
     bot.click_search()
     bot.apply_filtrations()
     bot.refresh() # A workaround to let our bot to grab the data properly
