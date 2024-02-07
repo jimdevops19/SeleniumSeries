ca7523c3bed0aba49490866b1f01b34de7d8259b
gauri3 <gauri.t@orangewood.co>
2024-02-07 18:06:54 +0530

    Refactor: Updated By.ID in Episode 2

--- 2/main.py
+++ 2/main.py
@@ -9,7 +9,7 @@ os.environ['PATH'] += r"C:/SeleniumDrivers"
 driver = webdriver.Chrome()
 driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
 driver.implicitly_wait(8)
-my_element = driver.find_element_by_id('downloadButton')
+my_element = driver.find_element(By.ID, 'downloadButton')
 my_element.click()
 
 WebDriverWait(driver, 30).until(
